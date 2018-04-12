4fa7d32bf07039a10b1d919bbf0e659ed1f5d68f
Alex Tuley <alex.intern@gaslight.co>
2018-04-12 13:30:10 -0400

    Start process of getting stats from a game

--- lib/track_nba_web/controllers/api/player_controller.ex
+++ lib/track_nba_web/controllers/api/player_controller.ex
@@ -7,8 +7,10 @@ defmodule TrackNbaWeb.PlayerController do
 
   def create(conn, %{"player_id" => player_id}) do
     stats = player_id
-    |> NbaEx.player_game_log
-    |> List.first
+    |> find_team()
+    |> find_game()
+    # |> retrieve_stats_for_player(game, player_id)
+
 
     player = NbaEx.players()
     |> Enum.find(fn(player) -> player.personId == player_id end)
@@ -16,5 +18,20 @@ defmodule TrackNbaWeb.PlayerController do
     new_player = Map.put_new(player, :stats, stats)
 
     render(conn, "player_stat.json", data: new_player)
+  end
+
+  defp find_team(player_id) do
+    NbaEx.players()
+    |> Enum.find(fn(player) -> player.personId == player_id end)
+    |> Map.get(:teamId)
+  end
+
+  defp find_game(team_id) do
+    scoreboard = NbaEx.scoreboard_for("20180410")
+
+    game = scoreboard.games
+    |> Enum.find(fn(game) -> game.vTeam.teamId == team_id || game.hTeam.teamId == team_id end)
+    |> IO.inspect
+
   end
 end
