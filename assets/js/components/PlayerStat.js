e3a7389c36ea9287ad74ed58391796752bc76cdf
Alex Tuley <alex.intern@gaslight.co>
2018-04-30 11:01:46 -0400

    Wrap up player card styling

--- assets/js/components/PlayerStat.js
+++ assets/js/components/PlayerStat.js
@@ -4,31 +4,50 @@ const PlayerStat = ({player}) => {
   return(
     <div className="col-md-5 player-stat__container">
       <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-5">
           <div className="row">
             <div className="player-pic"></div>
           </div>
           <div className="row player-name">
-            {player.firstName} {player.lastName}
+            <strong>{player.firstName} {player.lastName}</strong>
+          </div>
+          <div className="row game-stat">
+            PHL 100 - GSW 101
           </div>
         </div>
-        <div className="col-md-8">
-          <h2 className="main-stat">{player.stats.points}PTS {player.stats.assists}AST {player.stats.totReb}REB</h2>
-          <div className="row">
-            <div className="col-md-6">{player.stats.steals}</div>
-            <div className="col-md-6">STL</div>
+        <div className="col-md-7 u-border-left">
+          <div className="row main-stats">
+            <div className="col-md-4 u-border-right u-main-stat">
+              <h2>{player.stats.points}</h2>PTS
+            </div>
+            <div className="col-md-4 u-border-right u-main-stat">
+              <h2>{player.stats.assists}</h2>AST
+            </div>
+            <div className="col-md-4 u-main-stat">
+              <h2>{player.stats.totReb}</h2>REB
+            </div>
           </div>
-          <div className="row">
-            <div className="col-md-6">{player.stats.blocks}</div>
-            <div className="col-md-6">BLK</div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">{player.stats.turnovers}</div>
-            <div className="col-md-6">TO</div>
-          </div>
-          <div className="row">
-            <div className="col-md-6">{player.stats.fgm}/{player.stats.fga}</div>
-            <div className="col-md-6">{player.stats.fgp}</div>
+          <div className="secondary-stats">
+            <div className="row u-border-bottom u-main-stat">
+              <div className="col-md-6">{player.stats.steals}</div>
+              <div className="col-md-6">STL</div>
+            </div>
+            <div className="row u-border-bottom u-main-stat">
+              <div className="col-md-6">{player.stats.blocks}</div>
+              <div className="col-md-6">BLK</div>
+            </div>
+            <div className="row u-border-bottom u-main-stat">
+              <div className="col-md-6">{player.stats.turnovers}</div>
+              <div className="col-md-6">TO</div>
+            </div>
+            <div className="row u-border-bottom u-main-stat">
+              <div className="col-md-6">{player.stats.fgp}%</div>
+              <div className="col-md-6">FGP</div>
+            </div>
+            <div className="row u-main-stat">
+              <div className="col-md-6">{player.stats.fgm}/{player.stats.fga}</div>
+              <div className="col-md-6">FGM/FGA</div>
+            </div>
           </div>
         </div>
       </div>
