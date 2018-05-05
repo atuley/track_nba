6a0f5eaeaa6ff4a22d255711d9a8da1389186404
Alex Tuley <alex.intern@gaslight.co>
2018-05-05 16:47:07 -0400

    Refactor utility functions

--- lib/track_nba_web/utils.ex
+++ lib/track_nba_web/utils.ex
@@ -6,9 +6,8 @@ defmodule TrackNbaWeb.Utils do
   end
 
   def find_game(team_id) do
-    scoreboard = NbaEx.scoreboard()
-
-    result = scoreboard.games
+    result = NbaEx.scoreboard()
+    |> Map.get(:games)
     |> Enum.find(fn(game) -> game.vTeam.teamId == team_id || game.hTeam.teamId == team_id end)
 
     case result do
@@ -21,15 +20,16 @@ defmodule TrackNbaWeb.Utils do
   def retrieve_stats_for_player(game_id, player_id) do
     result = NbaEx.boxscore(current_date(), game_id)
 
-    l = Kernel.length(Map.get(result, :player_stats))
+    Map.get(result, :player_stats)
+    |> Kernel.length
+    |> has_stats?(result)
+  end
 
-    if l != 1 do
-      result
-      |> Map.get(:player_stats)
-      |> Enum.find(fn(player) -> player.personId == player_id end)
-    else
-      result
-    end
+  defp has_stats(1, result), do: result
+  defp has_stats(_length, result) do
+    result
+    |> Map.get(:player_stats)
+    |> Enum.find(fn(player) -> player.personId == player_id end)
   end
 
   def current_date do
