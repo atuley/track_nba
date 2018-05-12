2fa135fcad732cf0dfab72c921ac11aa03d66da2
Alex Tuley <alex.intern@gaslight.co>
2018-05-11 21:19:03 -0400

    Make player stat card mobile friendly

--- assets/js/components/PlayerStat.js
+++ assets/js/components/PlayerStat.js
@@ -4,7 +4,7 @@ const PlayerStat = ({player, playerLoading}) => {
   return(
     <div className="col-md-5 player-stat__container">
       <div className="row player-stat__border" style={{borderLeft: `8px solid ${player.teamColor}`}}>
-        <div className="col-md-5">
+        <div className="col-md-5 col-xs-5">
           <div className="row">
             <img className="player-pic" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${player.teamId}/2017/260x190/${player.personId}.png`}/>
           </div>
@@ -15,42 +15,42 @@ const PlayerStat = ({player, playerLoading}) => {
             PHL 100 - GSW 101
           </div>
         </div>
-        <div className="col-md-7 u-border-left">
+        <div className="col-md-7 col-xs-7 u-border-left">
           <div className="row main-stats">
-            <div className="col-md-4 u-border-right u-main-stat">
+            <div className="col-md-4 col-xs-4 u-border-right u-main-stat">
               <h2>{player.stats.points || 0}</h2>PTS
             </div>
-            <div className="col-md-4 u-border-right u-main-stat">
+            <div className="col-md-4 col-xs-4 u-border-right u-main-stat">
               <h2>{player.stats.assists || 0}</h2>AST
             </div>
-            <div className="col-md-4 u-main-stat">
+            <div className="col-md-4 col-xs-4 u-main-stat">
               <h2>{player.stats.totReb || 0}</h2>REB
             </div>
           </div>
           <div className="secondary-stats">
             <div className="row u-border-bottom u-main-stat">
-              <div className="col-md-6">{player.stats.steals || 0}</div>
-              <div className="col-md-6">STL</div>
+              <div className="col-md-6 col-xs-6">{player.stats.steals || 0}</div>
+              <div className="col-md-6 col-xs-6">STL</div>
             </div>
             <div className="row u-border-bottom u-main-stat">
-              <div className="col-md-6">{player.stats.blocks || 0}</div>
-              <div className="col-md-6">BLK</div>
+              <div className="col-md-6 col-xs-6">{player.stats.blocks || 0}</div>
+              <div className="col-md-6 col-xs-6">BLK</div>
             </div>
             <div className="row u-border-bottom u-main-stat">
-              <div className="col-md-6">{player.stats.turnovers || 0}</div>
-              <div className="col-md-6">TO</div>
+              <div className="col-md-6 col-xs-6">{player.stats.turnovers || 0}</div>
+              <div className="col-md-6 col-xs-6">TO</div>
             </div>
             <div className="row u-border-bottom u-main-stat">
-              <div className="col-md-6">{player.stats.fgp || 0}%</div>
-              <div className="col-md-6">FGP</div>
+              <div className="col-md-6 col-xs-6">{player.stats.fgp || 0}%</div>
+              <div className="col-md-6 col-xs-6">FGP</div>
             </div>
             <div className="row u-border-bottom u-main-stat">
-              <div className="col-md-6">{player.stats.fgm || 0}/{player.stats.fga || 0}</div>
-              <div className="col-md-6">FGM/FGA</div>
+              <div className="col-md-6 col-xs-6">{player.stats.fgm || 0}/{player.stats.fga || 0}</div>
+              <div className="col-md-6 col-xs-6">FGM/FGA</div>
             </div>
             <div className="row u-main-stat">
-              <div className="col-md-6">{player.stats.min || 0}</div>
-              <div className="col-md-6">min</div>
+              <div className="col-md-6 col-xs-6">{player.stats.min || 0}</div>
+              <div className="col-md-6 col-xs-6">min</div>
             </div>
           </div>
         </div>
