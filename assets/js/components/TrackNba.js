6c92ff8ee1adc11f6ddab89e3924d8456a400fe4
Alex Tuley <alex.intern@gaslight.co>
2018-06-06 19:46:30 -0400

    Remove duplicate code in TrackNba

--- assets/js/components/TrackNba.js
+++ assets/js/components/TrackNba.js
@@ -32,90 +32,58 @@ export default class TrackNba extends React.Component {
   }
 
   render() {
-    if (this.props.isLoading) {
+    const loading = () => {
       return (
-        <div>
-          <div className="row">
-            <input className="form-control" placeholder="Search for a player by name" type="text" onChange={searchPlayers.bind(this, 'players')}/>
-            <div className="player-search">
-              <table className="table">
-                <tbody>
-                  {_.map(this.state.players, (player) => {
-                    return (
-                      <tr className="search-border" key={player.personId} style={{borderLeft: `8px solid ${player.teamColor}`}}>
-                        <td className="col-md-2">
-                          <img className="search-player-pic" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${player.teamId}/2017/260x190/${player.personId}.png`}/>
-                        </td>
-                        <td className="col-md-8">
-                          <span className="player-name">
-                            {`${player.firstName} ${player.lastName}`}
-                          </span>
-                          <span className="player-pos">
-                            {player.pos}
-                          </span>
-                        </td>
-                        <td className="u-align-right col-md-2">
-                          <button className="add-button" disabled={player.isWatching} onClick={this.findPlayerStats.bind(this, player)}>
-                            <span>
-                              {player.isWatching ? "Watching" : "Watch"}
-                            </span>
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="row">
-            <img className="loading-gif" src="images/load.gif"/>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <div className="row">
-            <input className="form-control" placeholder="Search for a player by name" type="text" onChange={searchPlayers.bind(this, 'players')}/>
-            <div className="player-search">
-              <table className="table">
-                <tbody>
-                  {_.map(this.state.players, (player) => {
-                    return (
-                      <tr className="search-border" key={player.personId} style={{borderLeft: `8px solid ${player.teamColor}`}}>
-                        <td className="col-md-2">
-                          <img className="search-player-pic" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${player.teamId}/2017/260x190/${player.personId}.png`}/>
-                        </td>
-                        <td className="col-md-8">
-                          <span className="player-name">
-                            {`${player.firstName} ${player.lastName}`}
-                          </span>
-                          <span className="player-pos">
-                            {player.pos}
-                          </span>
-                        </td>
-                        <td className="u-align-right col-md-2">
-                          <button className="add-button" disabled={player.isWatching} onClick={this.findPlayerStats.bind(this, player)}>
-                            <span>
-                              {player.isWatching ? "Watching" : "Watch"}
-                            </span>
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className="row">
-            {_.map(this.props.playersWatching, (player) => {
-                return <PlayerStat key={player.personId} {...this.props} player={player}/>;
-            })}
-          </div>
-        </div>
+        <img className="loading-gif" src="images/load.gif"/>
       );
     }
+
+    const playersLoaded = () => {
+      return (
+        _.map(this.props.playersWatching, (player) => {
+            return <PlayerStat key={player.personId} {...this.props} player={player}/>;
+        })
+      );
+    }
+    return (
+      <div>
+        <div className="row">
+          <input className="form-control" placeholder="Search for a player by name" type="text" onChange={searchPlayers.bind(this, 'players')}/>
+          <div className="player-search">
+            <table className="table">
+              <tbody>
+                {_.map(this.state.players, (player) => {
+                  return (
+                    <tr className="search-border" key={player.personId} style={{borderLeft: `8px solid ${player.teamColor}`}}>
+                      <td className="col-md-2">
+                        <img className="search-player-pic" src={`https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/${player.teamId}/2017/260x190/${player.personId}.png`}/>
+                      </td>
+                      <td className="col-md-8">
+                        <span className="player-name">
+                          {`${player.firstName} ${player.lastName}`}
+                        </span>
+                        <span className="player-pos">
+                          {player.pos}
+                        </span>
+                      </td>
+                      <td className="u-align-right col-md-2">
+                        <button className="add-button" disabled={player.isWatching} onClick={this.findPlayerStats.bind(this, player)}>
+                          <span>
+                            {player.isWatching ? "Watching" : "Watch"}
+                          </span>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="row">
+          {this.props.isLoading ? loading() : playersLoaded()}
+        </div>
+      </div>
+    );
   }
 }
