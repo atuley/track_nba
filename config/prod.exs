965a4eec5e8ffb03dfe47eba6b2c9d40cc0afb26
Alex Tuley <alex.intern@gaslight.co>
2018-06-13 21:33:00 -0400

    config database

--- config/prod.exs
+++ config/prod.exs
@@ -20,6 +20,12 @@ config :track_nba, TrackNbaWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
+config :track_nba, TrackNbaWeb.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # Do not print debug messages in production
 config :logger, level: :info
 
