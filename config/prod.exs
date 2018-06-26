24432a7d508af779d1869dc2909932d7a2d7bc5b
Alex Tuley <alex.intern@gaslight.co>
2018-06-25 20:41:52 -0400

    Fix config file

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :track_nba, TrackNbaWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("PROD_SECRET_KEY")
 
-config :track_nba, TrackNbaWeb.Repo,
+config :track_nba, TrackNba.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   username: System.get_env("DATABASE_USERNAME"),
