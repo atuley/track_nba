a06d74bb278b0e1f4272916a155ae825bb81dbd5
Alex Tuley <alex.intern@gaslight.co>
2018-07-05 17:57:17 -0400

    Add more controller tests; mix format

--- test/track_nba_web/controllers/api/player_controller_test.exs
+++ test/track_nba_web/controllers/api/player_controller_test.exs
@@ -4,16 +4,44 @@ defmodule TrackNbaWeb.PlayerControllerTest do
 
   test "GET /api/players", %{conn: conn} do
     use_cassette "players" do
-      conn = get conn, "/api/players"
+      conn = get(conn, "/api/players")
       assert json_response(conn, 200)
     end
   end
-  # 
-  # test "POST /api/player/:player_id" do
-  #
-  # end
-  #
-  # test "POST /api/players/:player_ids" do
-  #
-  # end
+
+  describe "POST /api/player/:player_id" do
+    test "returns 200 with valid player_id" do
+      use_cassette "get_player" do
+        conn = post(build_conn(), "/api/player/201939")
+
+        assert json_response(conn, 200)
+      end
+    end
+
+    # test "returns 400 if player_id is invalid" do
+    #   use_cassette "get_player_error" do
+    #     conn = post(build_conn(), "/api/player/20193")
+    #
+    #     assert json_response(conn, 400)
+    #   end
+    # end
+  end
+
+  describe  "POST /api/players/:player_ids" do
+    test "return 200 with valid player_ids" do
+      use_cassette "get_players" do
+        conn = post(build_conn(), "/api/players/201939,203112")
+
+        assert json_response(conn, 200)
+      end
+    end
+
+    # test "returns 400 if player_id is invalid" do
+    #   use_cassette "get_player_error" do
+    #     conn = post(build_conn(), "/api/players/20193,20311")
+    #
+    #     assert json_response(conn, 400)
+    #   end
+    # end
+  end
 end
