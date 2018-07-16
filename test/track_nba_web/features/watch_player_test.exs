c872e55083a292508a603415c07e57be4f730625
Alex Tuley <alex.intern@gaslight.co>
2018-07-16 16:47:30 -0400

    Refresh pages in feature test to ensure caching is working properly

--- test/track_nba_web/features/watch_player_test.exs
+++ test/track_nba_web/features/watch_player_test.exs
@@ -9,12 +9,16 @@ defmodule TrackNbaWeb.WatchPlayerFeature do
     :ok
   end
 
-  test "player stats exist after watching a player" do
+  test "player stats exist after watching a player and refreshing the page" do
+    :timer.sleep(2000)
+    WatchPlayerPage.visit()
     assert WatchPlayerPage.has_stats?()
   end
 
-  test "player no longer exists after removing" do
+  test "player no longer exists after removing and refreshing the page" do
     WatchPlayerPage.remove_player()
+    assert_raise(Hound.NoSuchElementError, fn -> WatchPlayerPage.has_stats?() end)
+    WatchPlayerPage.visit()
     assert_raise(Hound.NoSuchElementError, fn -> WatchPlayerPage.has_stats?() end)
   end
 end
